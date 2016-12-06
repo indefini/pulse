bb1c606cd958354ebab215e7329b78d85db5d33c
Christophe Sadoine <chris@indefini.org>
2016-12-06 20:05:38 +0900

    move code from avion

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,19 @@
+#![feature(borrow_state)]
+#![feature(box_syntax)]
+#![feature(rc_counts)]
+
+extern crate dormin;
+extern crate uuid;
+extern crate libc;
+extern crate rustc_serialize;
+
+pub mod context;
+pub mod operation;
+pub mod ui;
+pub mod dragger;
+pub mod control;
+pub mod util;
+
 #[cfg(test)]
 mod tests {
     #[test]
